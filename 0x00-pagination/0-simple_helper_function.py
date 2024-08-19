4869267ce403e1e7bafd4ad43439767c6ca2d7f8
bishoyhanyhalim <bishoyhanyhalim@gmail.com>
2024-08-19 15:50:45 -0700

    all task

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -1,1 +1,12 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+"""Write a function named index range"""
+
+
+from typing import Tuple
+
+
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """
+    start index and an end the page
+    """
+    return ((page-1) * page_size, page_size * page)
