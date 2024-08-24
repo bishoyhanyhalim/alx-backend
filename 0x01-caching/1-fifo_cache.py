15126725ba241838f1c8843c31b95bb68aa854d1
bishoyhanyhalim <bishoyhanyhalim@gmail.com>
2024-08-23 18:53:21 -0700

    taks 1

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -1,1 +1,37 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+"""Create a class BasicCache task
+"""
+
+
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class FIFOCache(BaseCaching):
+    """_summary func
+    """
+
+    def __init__(self):
+        """_summary func
+        """
+        super().__init__()
+
+    def put(self, key, item):
+        """_summary func
+        """
+        if key is None or item is None:
+            pass
+        else:
+            if len(self.cache_data) >= BaseCaching.MAX_ITEMS \
+                    and key not in self.cache_data.keys():
+                first_key = next(iter(self.cache_data.keys()))
+                del self.cache_data[first_key]
+                print("DISCARD: {}". format(first_key))
+
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """return the value in func
+        """
+        if key is None or key not in self.cache_data.keys():
+            return None
+        return self.cache_data.get(key)
